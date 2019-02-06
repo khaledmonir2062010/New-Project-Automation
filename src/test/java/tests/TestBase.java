63f602e4765068b9d3f45c5c2d59237f541a56b4
khaled <khaled.monir@axa-egypt.com>
2019-02-06 11:55:01 +0200

    add headless testing with phantom js

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
@@ -13,61 +16,73 @@ import org.testng.annotations.Parameters;
 
 
 public class TestBase {
-	
-	
+
+
 	public static WebDriver driver;
-	
-	
+
+
 	@BeforeSuite
 	@Parameters({"browser"})
 	public void startDriver(@Optional("chrome")String browserName)
 	{
-		
-		
+
+
 		//Run Browsers Drivers
 		if (browserName.equalsIgnoreCase("Chrome")) {
-			
+
 			String chromePath=System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe";
-			
+
 			System.setProperty("webdriver.chrome.driver",chromePath);
-			
+
 			driver = new ChromeDriver();
-			
+
 		}
 		else if(browserName.equalsIgnoreCase("firefox"))
 		{
 			String FirefoxPath = System.getProperty("user.dir")+"\\Drivers\\geckodriver.exe";
-			
+
 			System.setProperty("webdriver.gecko.driver", FirefoxPath);
-			
+
 			driver =new FirefoxDriver();
+		}
+
+		else if (browserName.equalsIgnoreCase("headless")) 
+		{
+			DesiredCapabilities caps =new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			String PhatnomPath=System.getProperty("user.dir")+"\\Drivers\\phantomjs.exe";
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, PhatnomPath);
+			
+			String[] phatnomJsArgs= {"--web-security=no","--ignore-ssl-errors=yes"};
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phatnomJsArgs);
+			driver =new PhantomJSDriver(caps);
 		}
 		else if(browserName.equalsIgnoreCase("IE"))
 		{
 			String IEPath = System.getProperty("user.dir")+"\\Drivers\\IEDriverServer.exe";
-			
+
 			System.setProperty("webdriver.ie.driver", IEPath);
-			
+
 			driver =new InternetExplorerDriver();
 		}
-		
-		
-		
-		
+
+
+
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.navigate().to("https://www.nopcommerce.com/");	
 	}
-	
+
 	@AfterSuite
 	public void stopDriver()
 	{
-		
+
 		//driver.quit();
-		
+
 	}
-	
-	
-	
+
+
+
 
 }
